8abd2010aaccfc630981fc59fb60bb57c2397be2
lwb <lwb-2021@qq.com>
2025-08-11 23:20:31 +0800

    fix: fix the bug that I forgot to update the version

--- flake.nix
+++ flake.nix
@@ -52,7 +52,7 @@
           rustc = rustToolchain;
         }).buildRustPackage rec {
           pname = "${name}";
-          version = "0.1.0";
+          version = "0.2.0";
 
           src = ./.;
 
