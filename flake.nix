5b3d40c1e945dc4b1d1d3ff870c7a3fc552c37f9
lwb <lwb-2021@qq.com>
2025-10-06 21:04:41 +0800

    feat: init

---
+++ flake.nix
@@ -0,0 +1,104 @@
+{
+  description = "Minebak flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+    let
+      name = "minebak";
+      version = "1.0.0-alpha";
+    in
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+
+        rustNightly = pkgs.rust-bin.selectLatestNightlyWith (toolchain: toolchain.default.override {
+          extensions = [ "rust-src" ];
+        });
+
+        npmDeps = pkgs.fetchNpmDeps {
+          name = "${name}-${version}-npm-deps";
+          src = ./.;
+          hash = nixpkgs.lib.fakeSha256;
+        };
+
+      in
+      {
+        packages.default = pkgs.rustPlatform.buildRustPackage (finalAttrs: {
+          inherit name version;
+
+          src = ./.;
+
+          cargoHash = nixpkgs.lib.fakeSha256;
+
+          nativeBuildInputs = [
+            pkgs.cargo-tauri.hook
+            pkgs.nodejs
+            pkgs.npmHooks.npmConfigHook
+            pkgs.pkg-config
+            pkgs.gobject-introspection
+          ] ++ (with pkgs; lib.optionals stdenv.hostPlatform.isLinux [ wrapGAppsHook4 ]);
+
+          buildInputs = (with pkgs; [
+            at-spi2-atk
+            atkmm
+            cairo
+            gdk-pixbuf
+            glib
+            gtk3
+            harfbuzz
+            librsvg
+            libsoup_3
+            pango
+            webkitgtk_4_1
+            openssl
+          ]) ++ (with pkgs; lib.optionals stdenv.hostPlatform.isLinux [
+            glib-networking
+          ]);
+
+          cargoRoot = "src-tauri";
+          buildAndTestSubdir = finalAttrs.cargoRoot;
+
+          inherit npmDeps;
+        });
+
+        devShells.default = pkgs.mkShell {
+          inherit name;
+
+          nativeBuildInputs = with pkgs; [
+            pkg-config
+            gobject-introspection
+            rustNightly
+            cargo-tauri
+            nodejs
+          ];
+
+          buildInputs = with pkgs; [
+            at-spi2-atk
+            atkmm
+            cairo
+            gdk-pixbuf
+            glib
+            gtk3
+            harfbuzz
+            librsvg
+            libsoup_3
+            pango
+            webkitgtk_4_1
+            openssl
+          ];
+
+          shellHook = ''
+            export RUST_BACKTRACE=1
+          '';
+        };
+      }
+    );
+}
